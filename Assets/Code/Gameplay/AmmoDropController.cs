8c5ee8fb628e2d33e60802a1a4c4d66f2b0b1688
Jere <jere.joensuu@tuni.fi>
2021-11-03 15:12:54 +0200

    Refactor spawning animations

--- Assets/Code/Gameplay/AmmoDropController.cs
+++ Assets/Code/Gameplay/AmmoDropController.cs
@@ -3,28 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AmmoDropController : MonoBehaviour {
-    private ParticleSystem particle;
-    private SpriteRenderer sr;
     private Animation spawnAnimation;
     private BoxCollider2D boxCollider2D;
 
     void Start() {
         boxCollider2D = GetComponent<BoxCollider2D>();
         boxCollider2D.enabled = false;
-        sr = GetComponent<SpriteRenderer>();
-        sr.enabled = false;
-        particle = GetComponentInChildren<ParticleSystem>();
         spawnAnimation = GetComponent<Animation>();
 
         StartCoroutine(Spawn());
     }
 
     IEnumerator Spawn() {
-        particle.Play();
-
-        yield return new WaitForSeconds(particle.main.startLifetime.constantMax);
-        sr.enabled = true;
         spawnAnimation.Play();
+        yield return new WaitForSeconds(spawnAnimation.GetClip("AmmoSpawn").length);
         boxCollider2D.enabled = true;
     }
 }
--- Assets/Code/Gameplay/DropManager.cs
+++ Assets/Code/Gameplay/DropManager.cs
@@ -12,9 +12,12 @@ public class DropManager : MonoBehaviour {
     private int currentCooldown = 0;
     [Tooltip("Change of ammo drops spawning per second as percentage.")]
     [SerializeField] private float changeOfSpawn;
-
+    private ParticleSystem particleRays;
+    private ParticleSystem particleCircle;
     
     void Start() {
+        particleRays = transform.Find("DropAnimation").Find("Particle System").GetComponent<ParticleSystem>();
+        particleCircle = transform.Find("DropAnimation").Find("Center sphere").GetComponent<ParticleSystem>();
         
         foreach (Transform child in transform) {
             if (child.gameObject.GetComponent<PlatformController>() != null) {
@@ -45,13 +48,26 @@ public class DropManager : MonoBehaviour {
 
     void AttemptSpawn() {
         if (Random.Range(0, (int)(1 / changeOfSpawn)) == 0) {
-            GameObject spawnPlatform = Spawns[Random.Range(0,Spawns.Count)];
-            Instantiate(ammoDrop,
-                        new Vector2(Random.Range(spawnPlatform.transform.position.x - spawnPlatform.transform.localScale.x/2 + ammoDrop.transform.localScale.x/2,
-                                                    spawnPlatform.transform.position.x + spawnPlatform.transform.localScale.x/2)
-                                                ,spawnPlatform.transform.position.y + spawnPlatform.transform.localScale.y/2  + ammoDrop.transform.localScale.y/2),
-                        Quaternion.identity);
+            StartCoroutine(Spawn());
             currentCooldown = cooldown;
         }
+    }
+
+    IEnumerator Spawn() {
+        Debug.Log("spawning");
+        GameObject spawnPlatform = Spawns[Random.Range(0,Spawns.Count)];
+        Vector2 location = new Vector2(Random.Range(spawnPlatform.transform.position.x - spawnPlatform.transform.localScale.x/2 + ammoDrop.transform.localScale.x/2,
+                                                    spawnPlatform.transform.position.x + spawnPlatform.transform.localScale.x/2)
+                                       ,spawnPlatform.transform.position.y + spawnPlatform.transform.localScale.y/2  + ammoDrop.transform.localScale.y/2);
+
+        particleRays.transform.position = location;
+        particleCircle.transform.position = location;
+        particleRays.Play();
+        particleCircle.Play();
+
+        yield return new WaitForSeconds(particleCircle.main.startLifetime.constantMax);
+        
+        Instantiate(ammoDrop, location, Quaternion.identity);
+
     }
 }
