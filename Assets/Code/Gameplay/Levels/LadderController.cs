ac5dbc3700ed9a3fc4f7dc84ffcf1325a18ad676
jerejoensuu <jere.joensuu@tuni.fi>
2021-11-09 11:27:04 +0200

    Fix prefab problems in all scenes

--- Assets/Code/Gameplay/Levels/LadderController.cs
+++ Assets/Code/Gameplay/Levels/LadderController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class LadderController : MonoBehaviour {
     
-    
+    SpriteRenderer sp;
+    BoxCollider2D bc;
+    BoxCollider2D oneWay_bc;
 
     void Start() {
+        sp = GetComponent<SpriteRenderer>();
+        bc = GetComponent<BoxCollider2D>();
+        oneWay_bc = transform.Find("OneWayTop").GetComponent<BoxCollider2D>();
         
-        transform.Find("OneWayTop").GetComponent<BoxCollider2D>().offset = new Vector2(0, GetComponent<SpriteRenderer>().size.y/2 - transform.Find("OneWayTop").GetComponent<BoxCollider2D>().size.y/2);
-        GetComponent<BoxCollider2D>().size = new Vector2(0.3f, GetComponent<SpriteRenderer>().size.y);
-        GetComponent<BoxCollider2D>().offset = new Vector2(0f, 0.1f/2);
+        oneWay_bc.offset = new Vector2(0, sp.size.y/2 - oneWay_bc.size.y/2);
+        bc.size = new Vector2(0.3f, sp.size.y);
+        bc.offset = new Vector2(0f, 0.1f/2);
 
     }
 
