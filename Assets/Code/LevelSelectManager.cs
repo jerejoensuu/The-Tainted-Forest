6506aee90da52fda38c2a13f7c8bb54390a2304d
Jere <jere.joensuu@tuni.fi>
2021-11-28 01:13:52 +0200

    Fix bug with build

--- Assets/Code/LevelSelectManager.cs
+++ Assets/Code/LevelSelectManager.cs
@@ -17,14 +17,14 @@ public class LevelSelectManager : MonoBehaviour {
     public Button levelStartButton;
     
     void Awake() {
+        BuildButtons();
+        SetContainerWidth();
+        DisplayLevelInfo(1);
+
         trueLevels.AddRange(System.IO.Directory.GetFiles(System.IO.Directory.GetCurrentDirectory() + "\\Assets\\Scenes\\Levels", "*.unity"));
         if (trueLevels.Count > levels.Count) {
             Debug.Log("All levels not set to the list in Mainmenu: Canvas\\LevelSelectContainer -> LevelSelectManager!");
         }
-
-        BuildButtons();
-        SetContainerWidth();
-        DisplayLevelInfo(1);
     }
 
     void Update() {
