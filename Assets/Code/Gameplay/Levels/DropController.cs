acee04e01d88ddaa6939c853f0a6780128db7b75
Jere <jere.joensuu@tuni.fi>
2021-12-01 00:54:35 +0200

    Add item pickup sound

--- Assets/Code/Gameplay/Levels/DropController.cs
+++ Assets/Code/Gameplay/Levels/DropController.cs
@@ -8,8 +8,10 @@ public class DropController : MonoBehaviour {
     public int score;
     int spawnTime;
     bool destroying = false;
+    public AudioSource audioSrc;
 
     void Start() {
+        audioSrc = GetComponent<AudioSource>();
         spawnTime = GameObject.Find("PlatformAndDropManager").GetComponent<DropManager>().time;
     }
 
@@ -30,8 +32,17 @@ public class DropController : MonoBehaviour {
             collected = true;
             transform.root.Find("Player").GetComponent<PlayerController>().HandleDrops(transform.gameObject);
             AddToScore();
-            Destroy(transform.gameObject);
+            StartCoroutine(Destroy());
         }
+    }
+
+    IEnumerator Destroy() {
+        audioSrc.volume = ApplicationSettings.SoundVolume() * 0.1f;
+        audioSrc.Play();
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        yield return new WaitForSeconds(audioSrc.clip.length);
+        Destroy(transform.gameObject);
     }
 
     void AddToScore() {
